2877465c65531a9d9e6db41b85c0c21ec4b714f5
Justin Loroyé <justin@loroy.be>
2023-03-09 23:42:37 +0100

    remove initial and add readme

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import streamlit as st
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain import LLMChain
+from langchain import PromptTemplate
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-template = """
-    Tu es un assistant spécialisé dans le choix de nom de chien. Tu arrives toujours à trouver les meilleurs noms pour les chiens.
-    Ton but est de:
-    - Proposer une liste de 5 noms qui pourraient aller à un chien, en fonction de la description et des contraintes qui te sont données. Pour chaque nom, donne une explication du nom et pourquoi tu penses que cela irait bien au chien.
-    
-    Voici la description du chien: c'est un {size} chien {sex} de couleur {color}. {description}
-    {initial}
-    TA RESPONSE, SOUS FORME DE LISTE:
+forbidden_names = ["hitler", "shadow"]
+
+system_prompt_template = """
+    Tu es un assistant spécialisé dans le choix de nom de chien. Tu arrives toujours à trouver les meilleurs noms pour les chiens. Tes goûts sont inattendus et originaux. Les noms que tu proposes sont rares et unique. Tu ne donne jamais un nom figurant sur la liste des noms interdits.
+    Ton but est de proposer une liste de 20 noms qui pourraient aller à un chien, en fonction de la description et des contraintes qui te sont données.
+    les noms interdits sont [{forbidden}]
 """
 
-prompt = PromptTemplate(
-    input_variables=["size","sex", "color", "initial", "description"],
-    template=template,
-)
+human_prompt_template = """Voici la description du chien: c'est un {size} chien {sex} de couleur {color}. {description}"""
 
-def load_LLM():
+system_message_prompt = SystemMessagePromptTemplate.from_template(system_prompt_template)
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_prompt_template)
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+def load_Chat():
     """Logic for loading the chain you want to use should go here."""
     # Make sure your openai_api_key is set as an environment variable
-    llm = OpenAI(temperature=.7)
-    return llm
+    chat = ChatOpenAI(streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature=0)
+    return chat
 
-llm = load_LLM()
+chat = load_Chat()
 
-st.set_page_config(page_title="Nom d'un chien", page_icon=":dog:")
+
+st.set_page_config(layout="wide", page_title="Nom d'un chien", page_icon=":dog:")
 st.header("Nom d'un chien")
 st.markdown("Cet outil permet de choisir un nom qui va parfaitement à votre chien. Cet outil est possible grâce \
             à [LangChain](https://langchain.com/) et [OpenAI](https://openai.com) fait par \
@@ -33,36 +49,36 @@ st.markdown("Cet outil permet de choisir un nom qui va parfaitement à votre chi
 
 col1, col2 = st.columns(2)
 def get_color():
-    input_color = st.text_input(label="Couleur", placeholder="brun, noir, jaune", key="color_input")
+    input_color = st.text_input(label="Couleur", value="brun et noir", key="color_input")
     return input_color
-def get_initial():
-    input_initial = st.text_input(label="Initiale", placeholder="N'importe", key="initial_input")
-    if input_initial == "":
-        return ""
-    return "Le nom du chien doit commencer par "+input_initial
+
 def get_description():
-    input_description = st.text_input(label="Description", placeholder="mignon, gentil, calin", key="description_input")
+    input_description = st.text_area(label="Description", placeholder="il est mignon, gentil, et il aime les calins. Son nom doit commencer par un P.", key="description_input")
     return input_description
 
-def get_names():
-    print ("click on button: "+input_size +" "+ input_sex  +" "+ input_color  +" "+ input_initial  +" "+ input_description )
-    if input_size and input_sex and input_color and input_description: 
-        prompt_with_params = prompt.format(size=input_size, sex=input_sex, color=input_color, initial=input_initial, description=input_description)
-        col2.write(llm(prompt_with_params))
-    else:
-        print("at least one param missing: "+input_size +" "+ input_sex  +" "+ input_color  +" initiale:"+ input_initial  +" "+ input_description )
+def extract_names(text):
+    names = []
+    for word in text.split():
+        if word[0].isupper():
+            names.append(word.strip("'-"))
+    return names
 
+def get_names():
+    if input_size and input_sex and input_color and input_description:
+        chat_prompt_with_values = chat_prompt.format_prompt(size=input_size, sex=input_sex, color=input_color, description=input_description, forbidden=", ".join(forbidden_names))
+        print(chat_prompt_with_values.to_messages())
+        response = chat(chat_prompt_with_values.to_messages()).content
+        print(response)
+        forbidden_names.extend(extract_names(response))
+        col2.write(response)
+    else:
+        print("at least one param missing: "+input_size +" "+ input_sex  +" "+ input_color  +" "+ input_description )
+    
 with col1:
     st.markdown("## Parametres")
     input_size = st.radio('taille',['Petit', 'Moyen', 'Grand'])
     input_sex = st.radio('sexe',['Male', 'Femelle'])
     input_color = get_color()
-    input_initial = get_initial()
     input_description = get_description()
-
-
-
-with col2:
-    st.button("Generer", type='primary', help="Generer 5 noms de chiens", on_click=get_names)
-
+    st.button("Generer", type='primary', help="Generer 20 noms de chiens", on_click=get_names)
 
